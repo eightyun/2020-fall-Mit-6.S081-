e45840878b694e92d1403ca64bbfd9ee68578183
eightyun <879950144@qq.com>
2023-11-01 17:41:03 +0800

    finish lock

--- kernel/buf.h
+++ kernel/buf.h
@@ -1,6 +1,6 @@
 struct buf {
-  int valid;   // has data been read from disk?
-  int disk;    // does disk "own" buf?
+  int valid;   // has data been read from disk?   表示缓冲区是否包含块的副本
+  int disk;    // does disk "own" buf?  表示缓冲区内容是否已交给磁盘
   uint dev;
   uint blockno;
   struct sleeplock lock;
@@ -8,5 +8,6 @@ struct buf {
   struct buf *prev; // LRU cache list
   struct buf *next;
   uchar data[BSIZE];
+  uint timestamp;  // 时间戳
 };
 
--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,13 +21,20 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} kmem[NCPU];
 
 void
-kinit()
+kinit() // 
 {
-  initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+  char lockname[8] ;
+
+  for(int i = 0 ; i < NCPU ; i++)
+  {
+    snprintf(lockname, sizeof(lockname), "kmem_%d", i);
+    initlock(&kmem[i].lock, lockname);
+  }
+
+  freerange(end, (void*)PHYSTOP);  // 将内存添加到空闲列表中
 }
 
 void
@@ -44,7 +51,7 @@ freerange(void *pa_start, void *pa_end)
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
 void
-kfree(void *pa)
+kfree(void *pa) // 将一个内存页面添加（push）到空闲列表上
 {
   struct run *r;
 
@@ -55,26 +62,58 @@ kfree(void *pa)
   memset(pa, 1, PGSIZE);
 
   r = (struct run*)pa;
+  
+  push_off();
+  int id = cpuid() ;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  acquire(&kmem[id].lock);
+  r->next = kmem[id].freelist;
+  kmem[id].freelist = r;
+  release(&kmem[id].lock);
+
+  pop_off() ;
 }
 
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
 // Returns 0 if the memory cannot be allocated.
 void *
-kalloc(void)
+kalloc(void) // 从空闲页面列表中取出（pop）一个内存页面
 {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
+  push_off() ;
+  int id = cpuid() ;
+
+  acquire(&kmem[id].lock);
+  r = kmem[id].freelist;
+
   if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    kmem[id].freelist = r->next;
+  else
+  {
+    for(int i = 0 ; i < NCPU ; i++)
+    {
+      if(i == id) 
+        continue ;
+
+      acquire(&kmem[i].lock);
+      r = kmem[i].freelist;
+
+      if(r)
+      {
+        kmem[i].freelist = r->next;
+        release(&kmem[i].lock);
+        break ;
+      }
+
+      release(&kmem[i].lock);
+    }
+  }
+   
+  release(&kmem[id].lock);
+
+  pop_off() ;
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
